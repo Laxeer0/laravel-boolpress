38e8f47267da0a68a95f8d0f8ebd40376e82d13e
Giuseppe <laxeer0@gmail.com>
2021-02-08 16:58:09 +0100

    Update

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8"><h1>
+        <div class="col-md-8 text-align-center"><h1>
             @if($status == 'guest')
                 Ciao utente!
             @elseif($status == 'logged')
